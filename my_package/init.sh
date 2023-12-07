d580e134b38026f5c54c8e2874c5644619053583
Tsvika S <tsvika@github>
2023-12-07 17:48:14 +0200

    init verbosity

--- my_package/init.sh
+++ my_package/init.sh
@@ -5,21 +5,22 @@ set -e
 git init
 git add --all
 git reset "$0"
-git commit -m "initial commit"
+git commit -m "initial commit" -q
 
 # poetry install
 poetry env use python3.12
 poetry install -q
 git add poetry.lock
-git commit -m "chore: poetry install"
+git commit -m "chore: poetry install" -q
 
 # pre-commit update
-poetry run pre-commit autoupdate
+poetry run pre-commit autoupdate -j "$(nproc)"
 git add .pre-commit-config.yaml
-git commit -m "chore: pre-commit update"
+git commit -m "chore: pre-commit update" -q
 
 # pre-commit install
 poetry run pre-commit install
 
 echo "use 'gh repo create' to set this repo on GitHub"
+echo "use 'run pre-commit install-hooks' to install the pre-commit hooks"
 rm "$0"
