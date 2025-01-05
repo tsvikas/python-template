90fecaf897616155c1a177b4d1d8215d4b612bce
Tsvika Shapira <tsvikas@github>
2025-01-05 20:59:21 +0200

    add a cli with typer

---
+++ my_package/my_package/cli.py
@@ -0,0 +1,27 @@
+import typer
+
+
+app = typer.Typer()
+
+
+@app.callback()
+def callback():
+    """
+    Awesome Portal Gun
+    """
+
+
+@app.command()
+def shoot():
+    """
+    Shoot the portal gun
+    """
+    print("Shooting portal gun")
+
+
+@app.command()
+def load():
+    """
+    Load the portal gun
+    """
+    print("Loading portal gun")
