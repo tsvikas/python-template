46d1db2d3d6419efe906f532714efac0ddb6b537
Tsvika Shapira <tsvikas@gmail.com>
2025-01-06 00:30:19 +0200

    add manager

--- my-project/README.md
+++ my-project/README.md
@@ -6,14 +6,17 @@ import {{my_package}}
 ```
 
 ## Development
-* install git, python3.{{python_minor}}, poetry, poethepoet.
+* install git, python3.{{python_minor}}, {{manager}}, poethepoet.
 * git clone this repo
-* create a venv using `poetry env use python3.{{python_minor}}; poetry install`
-* enable pre-commit checks with `poetry run pre-commit install`
+* create a venv using {% if manager=="poetry" -%}
+  `poetry env use python3.{{python_minor}}; poetry install`
+  {%- elif manager=="uv" -%}
+  `uv python pin python3.{{python_minor}}; uv sync`
+  {%- endif %}
+* enable pre-commit checks with `{{manager}} run pre-commit install`
 * use `poe check` to verify code quality
 
 ## Build
-* install poetry-dynamic-versioning[plugin]
 * use `poe version` to see the current version
 * use `poe tag vX.Y.Z` to add a git tag. you still need to push it.
-* use `poetry build` to build
+* use `{{manager}} build` to build
--- my-project/init.sh
+++ my-project/init.sh
@@ -4,23 +4,30 @@ set -e
 # git init & initial commit
 git init -q
 git add --all
-git reset "init*.sh"
+git reset init.sh
 git commit -m "initial commit" -q
 
-# poetry install
+# install venv
+{%- if manager == "poetry" %}
 poetry env use python3.{{python_minor}}
 poetry install -q
 git add poetry.lock
 git commit -m "chore: poetry install" -q
+{%- elif manager == "uv" %}
+uv python pin python3.{{python_minor}}
+uv sync
+git add uv.lock .python-version
+git commit -m "chore: uv sync" -q
+{%- endif %}
 
 # pre-commit update
-poetry run pre-commit autoupdate -j "$(nproc)"
+{{manager}} run pre-commit autoupdate -j "$(nproc)"
 git add .pre-commit-config.yaml
 git commit -m "chore: pre-commit update" -q
 
 # pre-commit install
-poetry run pre-commit install > /dev/null
-poetry run pre-commit run --all-files
+{{manager}} run pre-commit install > /dev/null
+{{manager}} run pre-commit run --all-files
 
 echo "use 'gh repo create' to set this repo on GitHub"
-rm init*.sh
+rm init.sh
